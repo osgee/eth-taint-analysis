ca6d11c3dcedaa8624512f1aa12ea6198b04aad0
dabdevelop <develop@dab.vc>
2017-08-04 23:09:02 +0800

    Detect the exchange.

--- js/code.js
+++ js/code.js
@@ -16,32 +16,64 @@ $(function(){
   var transactions = getTransaction(address);
 
   var ether =  1000000000000000000;
-  var depth =  3;
+  var depth =  6;
 
   var nodes = [{data: { id: address, name: address}}];
   var edges = [];
+
+  var accounts = [];
+
+  //var exchanges = [];
+  var exchanges = ['0x57b174839cbd0a503b9dfcb655e4f4b1b47b3296', '0x70faa28a6b8d6829a4b1e649d26ec9a2a39ba413', '0x96fc4553a00c117c5b0bed950dd625d1c16dc894'];
 
 
   function walkerOne(address, callback){
     getTransaction(address, function(result){
       var queueAddresses = [];
       transactions = result['result'];
+      var out = 0;
+      var threshold = 10;
       for(j=0; j < transactions.length; j++){
         if(transactions[j].from === address){
-          if (queueAddresses.indexOf(transactions[j].to) >= 0) {
-          }else{
-            if(transactions[j].value > ether){
-              queueAddresses.push(transactions[j].to);
-              nodes.push({data: { id: transactions[j].to, name: transactions[j].to}})
-            }
-          }
           if(transactions[j].value > ether){
-            edges.push({data: {id: transactions[j].hash, weight: transactions[j].value/ether, source: transactions[j].from, target: transactions[j].to}});
-            console.log(transactions[j]);
+            out++;
+            if(out > threshold) break;
           }
         }
       }
-      callback(queueAddresses);
+      if(out > threshold){
+        exchanges.push(transactions[j].from);
+        edges.push({data: {id: address, weight: 0, label: "exchange", source: transactions[j].from, target: transactions[j].from}});
+        callback(queueAddresses);
+      } else {
+        for(j=0; j < transactions.length; j++){
+          if(transactions[j].from === address){
+            if (queueAddresses.indexOf(transactions[j].to) < 0) {
+              if(transactions[j].value > ether){
+                if(exchanges.indexOf(transactions[j].to) < 0){
+                  queueAddresses.push(transactions[j].to);
+                  nodes.push({data: { id: transactions[j].to, name: transactions[j].to}});
+                }else{
+                  nodes.push({data: { id: transactions[j].to, name: transactions[j].to}});
+                  edges.push({data: {id: transactions[j].hash, weight: transactions[j].value/ether, label: 'exchange: '  + transactions[j].value/ether, source: transactions[j].from, target: transactions[j].to}});
+                }
+                if(accounts.indexOf(transactions[j].to) < 0){
+                  accounts.push(transactions[j].to);
+                }
+              }
+            }
+
+            if(transactions[j].value > ether){
+              if(exchanges.indexOf(transactions[j].to) >= 0){
+                edges.push({data: {id: transactions[j].hash, weight: transactions[j].value/ether, label: 'exchange: '  + transactions[j].value/ether, source: transactions[j].from, target: transactions[j].to}});
+              }else{
+                edges.push({data: {id: transactions[j].hash, weight: transactions[j].value/ether, label: transactions[j].value/ether, source: transactions[j].from, target: transactions[j].to}});
+              }
+            }
+          }
+        }
+        callback(queueAddresses);
+      }
     });
   }
 
@@ -49,7 +81,9 @@ $(function(){
   function walkerQueue(queue, depth, callback){
     for(i = 0; i < queue.length; i++){
       walkerOne(queue[i], function(newQueue){
+        if(depth > 0 && newQueue.length > 0) {
           callback(newQueue, depth, callback);
+        }
       });
     }
   }
@@ -63,11 +97,12 @@ $(function(){
           if(depth > 0 && newQueue.length > 0){
             walkerQueue(newQueue, depth, callback);
           }
-          if(depth == 0){
             initCy();
-          }
+            console.log(accounts);
         });
       }
+      initCy();
+      console.log(accounts);
     });
 
   }
@@ -88,18 +123,18 @@ $(function(){
           .css({
             'content': 'data(name)',
             'text-valign': 'center',
-            'color': 'white',
-            'text-outline-width': 2,
-            'background-color': '#999',
+            'color': '#333',
             'text-outline-color': '#999'
           })
           .selector('edge')
           .css({
+            'label': "data(label)",
+            'edge-text-rotation': "autorotate",
             'curve-style': 'bezier',
             'target-arrow-shape': 'triangle',
             'target-arrow-color': '#ccc',
             'line-color': '#ccc',
-            'width': 1
+            'width': 3
           })
           .selector(':selected')
           .css({
